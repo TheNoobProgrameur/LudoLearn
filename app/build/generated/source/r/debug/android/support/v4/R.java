cd6303beac1875c21718799f6a40b1c826b3878a
Berthier <a.berthier@numericable.fr>
2019-03-17 15:53:58 -0400

    Mise en Place des Score

--- app/build/generated/source/r/debug/android/support/v4/R.java
+++ app/build/generated/source/r/debug/android/support/v4/R.java
@@ -67,14 +67,14 @@ public final class R {
         public static final int notify_panel_notification_icon_bg = 0x7f060070;
     }
     public static final class id {
-        public static final int action0 = 0x7f070006;
-        public static final int action_container = 0x7f07000e;
-        public static final int action_divider = 0x7f070010;
-        public static final int action_image = 0x7f070011;
-        public static final int action_text = 0x7f070017;
-        public static final int actions = 0x7f070018;
-        public static final int async = 0x7f07001d;
-        public static final int blocking = 0x7f070020;
+        public static final int action0 = 0x7f070007;
+        public static final int action_container = 0x7f07000f;
+        public static final int action_divider = 0x7f070011;
+        public static final int action_image = 0x7f070012;
+        public static final int action_text = 0x7f070018;
+        public static final int actions = 0x7f070019;
+        public static final int async = 0x7f07001e;
+        public static final int blocking = 0x7f070021;
         public static final int cancel_action = 0x7f070045;
         public static final int chronometer = 0x7f070051;
         public static final int end_padder = 0x7f070065;
@@ -86,41 +86,41 @@ public final class R {
         public static final int line1 = 0x7f070086;
         public static final int line3 = 0x7f070087;
         public static final int media_actions = 0x7f07008a;
-        public static final int normal = 0x7f07009a;
-        public static final int notification_background = 0x7f07009b;
-        public static final int notification_main_column = 0x7f07009c;
-        public static final int notification_main_column_container = 0x7f07009d;
-        public static final int right_icon = 0x7f0700b4;
-        public static final int right_side = 0x7f0700b5;
-        public static final int status_bar_latest_event_content = 0x7f0700d3;
-        public static final int text = 0x7f0700d8;
-        public static final int text2 = 0x7f0700d9;
-        public static final int time = 0x7f0700ea;
-        public static final int title = 0x7f0700eb;
+        public static final int normal = 0x7f070098;
+        public static final int notification_background = 0x7f070099;
+        public static final int notification_main_column = 0x7f07009a;
+        public static final int notification_main_column_container = 0x7f07009b;
+        public static final int right_icon = 0x7f0700a6;
+        public static final int right_side = 0x7f0700a7;
+        public static final int status_bar_latest_event_content = 0x7f0700c5;
+        public static final int text = 0x7f0700ca;
+        public static final int text2 = 0x7f0700cb;
+        public static final int time = 0x7f0700db;
+        public static final int title = 0x7f0700dc;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f080002;
         public static final int status_bar_notification_info_maxnum = 0x7f080004;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f090028;
-        public static final int notification_action_tombstone = 0x7f090029;
-        public static final int notification_media_action = 0x7f09002a;
-        public static final int notification_media_cancel_action = 0x7f09002b;
-        public static final int notification_template_big_media = 0x7f09002c;
-        public static final int notification_template_big_media_custom = 0x7f09002d;
-        public static final int notification_template_big_media_narrow = 0x7f09002e;
-        public static final int notification_template_big_media_narrow_custom = 0x7f09002f;
-        public static final int notification_template_custom_big = 0x7f090030;
-        public static final int notification_template_icon_group = 0x7f090031;
-        public static final int notification_template_lines_media = 0x7f090032;
-        public static final int notification_template_media = 0x7f090033;
-        public static final int notification_template_media_custom = 0x7f090034;
-        public static final int notification_template_part_chronometer = 0x7f090035;
-        public static final int notification_template_part_time = 0x7f090036;
+        public static final int notification_action = 0x7f090026;
+        public static final int notification_action_tombstone = 0x7f090027;
+        public static final int notification_media_action = 0x7f090028;
+        public static final int notification_media_cancel_action = 0x7f090029;
+        public static final int notification_template_big_media = 0x7f09002a;
+        public static final int notification_template_big_media_custom = 0x7f09002b;
+        public static final int notification_template_big_media_narrow = 0x7f09002c;
+        public static final int notification_template_big_media_narrow_custom = 0x7f09002d;
+        public static final int notification_template_custom_big = 0x7f09002e;
+        public static final int notification_template_icon_group = 0x7f09002f;
+        public static final int notification_template_lines_media = 0x7f090030;
+        public static final int notification_template_media = 0x7f090031;
+        public static final int notification_template_media_custom = 0x7f090032;
+        public static final int notification_template_part_chronometer = 0x7f090033;
+        public static final int notification_template_part_time = 0x7f090034;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0c0021;
+        public static final int status_bar_notification_info_overflow = 0x7f0c0023;
     }
     public static final class style {
         public static final int TextAppearance_Compat_Notification = 0x7f0d00fa;
---
+++ app/src/main/java/progmobile/ludolearn/ResultatMathsActivity.java
@@ -0,0 +1,8 @@
+package progmobile.ludolearn;
+
+/**
+ * Created by abert on 21/02/2019.
+ */
+
+public class ResultatMathsActivity {
+}
